9c45bb4d6e75f7242013371ffafb04eb7d193799
BuildTools <marco.firsteye@gmail.com>
2017-02-06 14:25:32 -0800

    Hand moves around once, finishes out of screen

--- GameObject.py
+++ GameObject.py
@@ -1,17 +1,21 @@
 import Queue, math
 class GameObject:
-    def __init__(self, image, height, speed):
+    def __init__(self, image, height, speed, end):
         self.speed = float(speed)
         self.image = image
         self.pos = image.get_rect().move(0, height)
         self.path = Queue.Queue()
         self.goal = [0,height]
+        self.end_pos = end
     def move(self):
-        if(abs(self.pos[0] - self.goal[0]) < (self.speed + 1) and \
-           abs(self.pos[1] - self.goal[1]) < (self.speed + 1)):
-            self.goal = self.path.get()
-
-        if(self.goal[1] - self.pos[1] != 0):
+        if (not self.path.empty()):
+            if(abs(self.pos[0] - self.goal[0]) < (self.speed + 1) and \
+               abs(self.pos[1] - self.goal[1]) < (self.speed + 1)):
+                self.goal = self.path.get()
+        else:
+            self.goal = self.end_pos
+            print "Done Drawing"
+        if(self.goal[0] - self.pos[0] != 0):
             angle = math.atan((float(self.goal[1]) - float(self.pos[1])) /\
                                 (float(self.goal[0]) - float(self.pos[0])))
         else:
@@ -30,8 +34,10 @@ class GameObject:
         self.pos = self.pos.move(x, y)
         #if self.pos.right > 1280:
         #    self.pos.left = 0
-    def addPoint(self, point):
+    def add_point(self, point):
         self.path.put(point)
+    def add_lines(self, point):
+        #self.path.put(point)
 
 
         """
--- renderer.py
+++ renderer.py
@@ -1,50 +1,54 @@
 from bs4 import BeautifulSoup
 import sys, pygame, GameObject
+#========================================== Pygae Stuff
 pygame.init()
 
-# Define the colors we will use in RGB format
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
 
-size = width, heignt = 1280, 800
+size = width, height = 1280, 800
 speed = [2, 2]
 black = 0, 0, 0
-movement_path = []
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Example code for the draw module")
-
-#Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
 handimage = pygame.image.load('hand.png').convert_alpha()
-hand = GameObject.GameObject(handimage, 10, 5)
+hand = GameObject.GameObject(handimage, 10, 5, size)
+
+#========================================== BeautifulSoup Stuff
 f = file("ex.svg", "r")
 html_doc = f.read();
 f.close()
 svg = BeautifulSoup(html_doc, 'html.parser')
-def drawLine(pos1, pos2):
-    pygame.draw.aaline(screen, WHITE, pos1, pos2, 5)
-    hand.addPoint(pos1)
-    hand.addPoint(pos2)
+
+#========================================== Global Stuff
+lines = [] #Lines that will be rendered every frame
+
+#========================================== Main Functions
+def save_line(pos1, pos2):
+    #pygame.draw.aaline(screen, WHITE, pos1, pos2, 5)
+    hand.add_point(pos1)
+    hand.add_point(pos2)
+    lines.append([pos1,pos2]) #[[x0,y0],[x1,y1]]
     
-def renderPolyline(poly):
+    
+def make_polyline(poly):
     points = poly['points'].replace("\n\t","").split(" ")
     pointlist = []
     for p in points[0:-1]:
         p = p.split(",")
         pointlist.append([int(float(p[0])), int(float(p[1]))])
-
     prevp = pointlist[0]
-
     for p in pointlist[1:]:
-        drawLine(prevp, p)
+        save_line(prevp, p)
         prevp = p
 
-def renderPath(path,steps):
+def make_path(path,steps):
     curves = path['d'].replace("c","cp,")\
                     .replace("C","cP,")\
                     .replace("M","")\
@@ -86,7 +90,7 @@ def renderPath(path,steps):
                 (3*(1-t)*(1-t)*t*y1) +\
                 (3*(1-t)*t*t*y2) +\
                 (t*t*t*y3)
-            drawLine(prevp, [x, y])
+            save_line(prevp, [x, y])
             prevp = [x, y]
             
 
@@ -95,25 +99,30 @@ def renderSVG(soup):
         n = tag.name
         if(n == "polyline"):
             #print "Poly"
-            renderPolyline(tag)
+            make_polyline(tag)
         if(n == "line"):
             #print "Line"
             pass
         elif(n =="path"):
             #print "Path"
-            renderPath(tag, 20)
+            make_path(tag, 20)
         elif(n =="rect"):
             #print "Rect"
             pass
 
+def render_lines(lines):
+    for line in lines:
+        pygame.draw.aaline(screen, WHITE, line[0], line[1], 5)
 
+renderSVG(svg)
+hand.add_lines(lines)
+    
 while not done:
  
     # This limits the while loop to a max of 10 times per second.
     # Leave this out and we will use all CPU we can.
     screen.fill(BLACK)
-    renderSVG(svg)
-    
+    render_lines(lines)
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done=True # Flag that we are done so we exit this loop
